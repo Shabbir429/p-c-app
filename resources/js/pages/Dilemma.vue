25701d743765364175ab1901830b8c88d3de7746
Shabbirali Bhoraniya <shabbirali6812@gmail.com>
2023-07-11 18:33:48 +0530

    done

--- resources/js/pages/Dilemma.vue
+++ resources/js/pages/Dilemma.vue
@@ -5,30 +5,34 @@
                 <h1 class="text-capitalize">{{ list.title }}</h1>
             </div>
             <div class="col-sm-12">
-                <img @click="edit()" class="ms-auto p-2 pb-3" width="80" height="80" src="https://img.icons8.com/external-others-inmotus-design/100/external-Edit-virtual-keyboard-others-inmotus-design.png" alt="external-Edit-virtual-keyboard-others-inmotus-design"/>
+                <img @click="edit()" class="ms-auto p-2 pb-2" width="70" height="70" src="https://img.icons8.com/external-others-inmotus-design/100/external-Edit-virtual-keyboard-others-inmotus-design.png" alt="external-Edit-virtual-keyboard-others-inmotus-design"/>
             </div>
             <div class="box col col-sm-12 d-flex px-2 py-2 text-center">
-                <span style="background:rgb(252, 3, 132);" class="d-inline p-2 w-100 ">+{{per.prosper}}%</span>
-                <span style="background:rgb(138, 184, 254);" class="d-inline p-2 w-100">-{{ per.consper}}%</span>
+                <span :style="{ width: `${per.prosper}%`, background: 'rgb(252, 3, 132)' }" class="d-inline p-2 ">+{{per.prosper}}%</span>
+                <span :style="{ width: `${per.consper}%`, background: 'rgb(138, 184, 254)' }" class="d-inline p-2">-{{ per.consper}}%</span>
             </div>
         </div>
-        <div class="row">
+        <div class="row mt-3">
             <div class="col">
                 <h3>Pros</h3>
-                <div class="col-sm-12 mt-3 shadow d-flex flex-row mb-2" v-for="item in pros" :key="item.id">
-                    <div class="circle ">{{ item.importance}}</div>
-                    <div class="p-2 text-break">{{ item.argument}}</div>
-                </div>
+                <button @click="argumentedit(item)" class="col-sm-12">
+                    <div class="col-sm-12 mt-3 shadow d-flex flex-row mb-2" v-for="item in pros" :key="item.id">
+                        <div class="circle ">{{ item.importance}}</div>
+                        <div class="p-2 text-break">{{ item.argument}}</div>
+                    </div>
+                </button>
             </div>
             <div class="col">
                 <h3>Cons</h3>
-                <div class="col-sm-12 mt-3 shadow d-flex flex-row mb-2" v-for="item in cons" :key="item.id">
-                    <div class="circle" style="background: rgb(102, 162, 251);">{{ item.importance}}</div>
-                    <div class="p-2 text-break">{{ item.argument}}</div>
-                </div>
+                <button @click="argumentedit(item)" class="col-sm-12 ">
+                    <div class="col-sm-12 mt-3 shadow d-flex flex-row mb-2" v-for="item in cons" :key="item.id">
+                        <div class="circle" style="background: rgb(138, 184, 254);">{{ item.importance}}</div>
+                        <div class="p-2 text-break">{{ item.argument}}</div>
+                    </div>
+                </button>
             </div>
         </div>
-        <button class="btn" type="button" @click="argument">ADD ARGUMENT</button>
+        <button class="btn mbtn" type="button" @click="argument">ADD ARGUMENT</button>
     </div>
 </template>
 
@@ -43,12 +47,12 @@ export default{
             total:{
                 ptotal:0,
                 ctotal:0,
-                total:0,
             },
             per:{
                 prosper:50,
                 consper:50,
-            }
+            },
+            addition:0,
         };
     },
     mounted() {
@@ -59,13 +63,14 @@ export default{
         this.findtitle();
         this.findpros();
         this.findcons();
-        this.total.total=this.total.ptotal+this.total.ctotal;
-        this.persantage();
     },
     methods:{
         persantage(){
-            this.per.prosper = (this.total.ptotal / this.total.total) * 100;
-            this.per.consper = (this.total.ctotal / this.total.total) * 100;
+            this.per.prosper = ((this.total.ptotal / this.addition) * 100).toFixed(1);
+            this.per.consper = ((this.total.ctotal / this.addition) * 100).toFixed(1);
+            // if(this.per.consper==0.0){
+            //     this.per.consper="";
+            // }
         },
         edit(){
                 const data = this.list.title;
@@ -78,7 +83,9 @@ export default{
         findtitle(){
             axios.get("/api/findtitle/" + this.list.id)
             .then((response) => {
-            this.list = response.data;
+                this.list = response.data;
+                this.findpros();
+                this.findcons();
             })
             .catch((error) => {
                 console.log(error);
@@ -88,7 +95,11 @@ export default{
             axios.get("/api/findpros/" + this.list.id)
             .then((response) => {
             this.pros = response.data.data;
-            this.total.ptotal = response.data.sum;
+            this.total.ptotal = parseInt(response.data.sum);
+            if(this.total.ptotal!=0  || this.total.ctotal!=0){
+                this.addition = this.total.ptotal + this.total.ctotal; // Calculate the total here
+                this.persantage();
+            }
             })
             .catch((error) => {
                 console.log(error);
@@ -98,7 +109,14 @@ export default{
             axios.get("/api/findcons/" + this.list.id)
             .then((response) => {
             this.cons = response.data.data;
-            this.total.ctotal = response.data.sum;
+            this.total.ctotal = parseInt(response.data.sum);
+            if(this.total.ptotal!=0 & this.total.ctotal!=0){
+                this.addition = this.total.ptotal + this.total.ctotal; // Calculate the total here
+                this.persantage();
+            }
+            // console.log(this.total.ptotal);
+            // console.log(this.total.ctotal);
+            // console.log(this.addition);
             })
             .catch((error) => {
                 console.log(error);
@@ -109,6 +127,13 @@ export default{
             this.$router.push({
                 name: 'Argument',
                 query: { id },
+            });
+        },
+        argumentedit(item){
+            // const item = item;
+            this.$router.push({
+                name: 'Argument',
+                query: { item },
             });
         },
     },
@@ -143,7 +168,7 @@ export default{
     height:auto;
     border-radius:0.5em;
 }
-button{
+.mbtn{
     border: 0.1em solid;
     width:10em;
     margin-top:10em;
